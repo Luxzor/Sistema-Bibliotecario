c7de01d12de72cc1871c9fcdfbcb0f09bde6bea5
Joan Miam <cesarjoanmiamchan@gmail.com>
2024-12-02 22:43:29 -0600

    Correcion de Getters y Setter

---
+++ src/main/java/mycompany/SpringPruebaMVC/entities/ActionType.java
@@ -0,0 +1,12 @@
+// src/main/java/mycompany/SpringPruebaMVC/model/entities/ActionType.java
+
+package mycompany.SpringPruebaMVC.model.entities;
+
+public enum ActionType {
+    LOAN,
+    RETURN,
+    NEW_BOOK,
+    DELETE_BOOK,
+    NEW_USER,
+    DELETE_USER
+}
